38aeb1638b7ca07e57e25da3c7f3dc0bf2604c51
OvOpenguin <rosebox14@gmail.com>
2025-07-15 11:51:01 +0800

    0715-2

--- js0715/lott2.js
+++ js0715/lott2.js
@@ -4,118 +4,63 @@ let sortBefore = document.getElementById("sortBefore");
 // 排序後
 let sortAfter = document.getElementById("sortAfter");
 // 第二區號碼
-let secNo = document.getElementById("secNo");
-// 存放號碼的陣列
-let arrLott = [];
+let secShow = document.getElementById("secNo");
 
-// 呼叫函式
-function lott(tp, max, sec) {
-    if (tp === 1) {
-        // alert("威力彩");
-        // 第一區：1-38取6個
-        // 建立存放號碼的空陣列
-        // let lott = [];
+// 彩票
+// getlott(類型,第一區最大值,第一區取幾個號碼,第二區最大值)
+function getlott(tp, firstMax, firstNo, secMax) {
+    // 第一區：1-38取6個
+    // 建立存放號碼的空陣列
+    let lott = [];
 
-        // while
-
-        while (1) {
-            // 檢查是已經有6個號碼
-            if (lott.length == 6) {
-                break;
-            }
-            getNo(max);
-            // // 亂數取號碼
-            // let lottNo = Math.floor(Math.random() * max) + 1;
-            // // 檢查是否重覆
-            // if (!lott.includes(lottNo)) {   // 沒有重覆
-            //     lott.push(lottNo);  // 增加號碼到陣列中            
-            // }
-        }
-
-        // 未排序
-        sortBefore.textContent = "排序前號碼:" + lott;
-        // 排序
-        lott.sort((a, b) => {
-            return a - b;
-        })
-        sortAfter.textContent = "排序後號碼:" + lott;
-        // 第二區：1-8取1個
-        secNo.textContent = "第二區號碼:" + (Math.floor(Math.random() * sec) + 1);
-
-    } else if (tp === 2) {
-        // alert("大樂透");
-        // 第一區：1-49取6個
-        // 建立存放號碼的空陣列
-        let lott = [];
-
-        // while
-
-        while (1) {
-            // 檢查是否已經有7個號碼
-            if (lott.length == 7) {
-                break;
-            }
-
-            // 亂數取號碼
-            let lottNo = Math.floor(Math.random() * max) + 1;
-            // 檢查是否重覆
-            if (lott.includes(lottNo) == false) {   // 沒有重覆
-                lott.push(lottNo);  // 增加號碼到陣列中            
-            }
-        }
-
-        // .pop() 取出陣列最後一個值後，並移除
-        secNo.textContent = "特別號：" + lott.pop();
-
-        // 未排序
-        sortBefore.textContent = "排序前號碼:" + lott;
-        // 排序
-        lott.sort((a, b) => {
-            return a - b;
-        })
-        sortAfter.textContent = "排序後號碼:" + lott;
-
-    } else if (tp === 3) {
-        // alert("今彩539");
-        // 第一區：1-39取5個
-        // 建立存放號碼的空陣列
-        let lott = [];
-
-        // while
-
-        while (1) {
-            // 檢查是已經有5個號碼
-            if (lott.length == 5) {
-                break;
-            }
-
-            // 亂數取號碼
-            let lottNo = Math.floor(Math.random() * max) + 1;
-            // 檢查是否重覆
-            if (lott.includes(lottNo) == false) {   // 沒有重覆
-                lott.push(lottNo);  // 增加號碼到陣列中            
-            }
-        }
-
-        // 未排序
-        sortBefore.textContent = "排序前號碼:" + lott;
-        // 排序
-        lott.sort((a, b) => {
-            return a - b;
-        })
-        sortAfter.textContent = "排序後號碼:" + lott;
-
-        // 今彩沒有第二區，所以清除
-        secNo.textContent = "";
+    if (tp == 2) {
+        firstNo = firstNo + 1;
     }
-}
 
+    while (1) {
+        // 第一區
+        // 是否為大樂透，若是，則總數+1，作為特別號。
+        // 檢查是已經有 N 個號碼
+        if (lott.length == firstNo) {
+            break;
+        }
 
-function getNo(max){
-    // 亂數取號碼
-    let lottNo = Math.floor(Math.random() * max) + 1;
-    // 檢查是否重覆
-    if (!arrLott.includes(lottNo)) {   // 沒有重覆
-        arrLott.push(lottNo);  // 增加號碼到陣列中            
+        // 亂數取號碼
+        let lottNo = Math.floor(Math.random() * firstMax) + 1;
+        // 檢查是否重覆
+        if (!lott.includes(lottNo)) {   // 沒有重覆
+            lott.push(lottNo);  // 增加號碼到陣列中            
+        }
     }
+
+
+    // 第二區
+    secShow.textContent = "";
+    if (tp == 1) {
+        secShow.textContent = "第二區號碼:" + (Math.floor(Math.random() * secMax) + 1);
+    } else if (tp == 2) {
+        secShow.textContent = "特別號：" + lott.pop();
+    }
+
+    // 第二區 (test)
+    // if (tp === 1) {
+    //     // 第二區：1-8取1個
+    //     secShow.textContent = "第二區號碼:" + (Math.floor(Math.random() * secMax) + 1);
+    // } else if (tp === 2) {
+    //     // 取出陣列最後一個值後，並移除
+    //     secShow.textContent = "特別號：" + lott.pop();
+
+    // } else if (tp === 3) {
+    //     // 今彩沒有第二區，所以清除
+    //     secShow.textContent = "";
+    // }
+
+    // 未排序
+    sortBefore.textContent = "排序前號碼:" + lott;
+    // 排序
+    lott.sort((a, b) => {
+        return a - b;
+    })
+    sortAfter.textContent = "排序後號碼:" + lott;
+
 }
